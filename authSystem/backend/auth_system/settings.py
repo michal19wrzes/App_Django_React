bc8ff1de78d69a46ffd7a53fce4f5a8ae83b7caa
Nogal <edvnoga@ne-europe.kronospan.int>
2022-03-29 19:40:58 +0200

    websocket client connected

--- authSystem/backend/auth_system/settings.py
+++ authSystem/backend/auth_system/settings.py
@@ -95,7 +95,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 
 EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSS')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 EMAIL_USE_TLS = True
 
